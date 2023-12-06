d13e798371c4cf63164d2c5a564aaa1ea093d143
Perska <perska.weegee@gmail.com>
2023-12-06 07:43:20 +0200

    day6

---
+++ AoC2023/Days/Day06.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace AoC2023
+{
+	partial class Program
+	{
+		// [UseSRL] // Uncomment if you wanna use SuperReadLine
+		[NoTrailingNewLine] // Uncomment to not include an extra blank line in the input at the end
+		static void Day06(List<string> input)
+		{
+			var times = input[0].SplitToStringArray(":", true)[1].SplitToIntArray(" ");
+			var distances = input[1].SplitToStringArray(":", true)[1].SplitToIntArray(" ");
+			long truetime = long.Parse(input[0].SplitToStringArray(":", true)[1].Replace(" ", ""));
+			long truedistance = long.Parse(input[1].SplitToStringArray(":", true)[1].Replace(" ", ""));
+
+			long margin = 1;
+			for (int i = 0; i < times.Length; i++)
+			{
+				margin = margin * race(times[i], distances[i]);
+			}
+
+			WriteLine(margin);
+
+			int race(long time, long distance)
+			{
+				int winners = 0;
+				for (int j = 0; j <= time; j++)
+				{
+					if (distance < (time - j) * j) winners++;
+				}
+				return winners;
+			}
+
+			WriteLine(race(truetime, truedistance));
+		}
+	}
+}
